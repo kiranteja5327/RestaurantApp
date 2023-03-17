f2fd5b345762c330a0b3c298797cfe3667b18a1d
kiranteja5327 <kiranteja5327@gmail.com>
2023-03-17 21:01:43 +0530

    first commit

---
+++ in/teja/main/Main.java
@@ -0,0 +1,24 @@
+package in.teja.main;
+
+import java.util.Scanner;
+
+import in.teja.restaurant.Restaurantowner;
+
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+	
+		
+		System.out.println("*******TEJA'S GRAND RESTAURANT******");
+		System.out.println("NOTE: NON-VEG ONLY & TAKE-AWAY");
+		Restaurantowner obj1=new Restaurantowner();
+		obj1.owner();
+		
+		System.out.println();
+	
+
+	}
+
+}
